79bb0946c4777fa337659153f0d480f2be22e2bc
Unknown <kevinquist@zeelandnet.nl>
2018-03-27 18:55:17 +0200

    logs maken

--- SmartMotionWeb-app/src/app/form/form.component.ts
+++ SmartMotionWeb-app/src/app/form/form.component.ts
@@ -3,13 +3,30 @@ import { Component } from '@angular/core';
 import { Info }    from '../info';
 
 import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+  settingslogs = "";
+  jsonresponse;
+
+  ngOnInit(): void {
+    this.http.get('http://192.168.178.234:42069/getsettingslog').subscribe(
+      res => {
+        this.jsonresponse = res;
+        console.log(res);
+        this.parseJson();
+
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 
   constructor(private http: HttpClient){
   } 
@@ -21,7 +38,7 @@ export class FormComponent {
   onSubmit() { this.submitted = true; this.sendData(); }
 
   sendData() {
-    this.http.post('http://192.168.11.25:42069/sendvalues',this.model).subscribe(
+    this.http.post('http://192.168.178.234:42069/sendsettings',this.model).subscribe(
       res => {
         console.log(res);
       },
@@ -29,5 +46,15 @@ export class FormComponent {
         console.log(err);
       }
     );
-  } 
+  }
+  
+  parseJson(){
+    for(var j in this.jsonresponse.response){
+    console.log(j);
+    this.settingslogs += this.jsonresponse.response[j].timeout + ' ';
+    this.settingslogs += this.jsonresponse.response[j].mintemp + ' ';
+    this.settingslogs += this.jsonresponse.response[j].maxtemp + ' ';
+    this.settingslogs += this.jsonresponse.response[j].time.slice(0,19).replace('T',' ')    + '<br/>';
+    }
+  }
 }
